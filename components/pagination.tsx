eda3d4614ebe843d4207548cbeae68d0bc6b6a8c
Yuki Y <yukidmy@gmail.com>
2024-04-02 17:32:08 +0900

    Apply MAX_PAGINATION_BUTTONS

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -3,6 +3,10 @@ import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 import type { NextPage } from "next";
 
 export const ARTICLES_PER_PAGE = 6;
+const MAX_PAGINATION_BUTTONS = 5;
+
+const range = (start: number, end: number) =>
+  [...Array(end - start + 1)].map((_, i) => start + i);
 
 const formatPrefix = (prefix: string): string => {
   if (!prefix.startsWith("/")) {
@@ -14,6 +18,27 @@ const formatPrefix = (prefix: string): string => {
   return prefix;
 };
 
+const generatePageNumbersToShow = (
+  max_page: number,
+  focused: number
+): Array<number> => {
+  if (max_page <= MAX_PAGINATION_BUTTONS) {
+    return range(1, max_page);
+  }
+  const threthold = Math.floor(MAX_PAGINATION_BUTTONS / 2);
+  if (focused <= threthold || max_page - threthold < focused) {
+    return range(1, threthold)
+      .concat([0])
+      .concat(range(max_page - threthold + 1, max_page));
+  } else {
+    return range(1, threthold - 1)
+      .concat([0])
+      .concat([focused])
+      .concat([0])
+      .concat(range(max_page - threthold + 2, max_page));
+  }
+};
+
 export const Pagination: NextPage<{
   base: string;
   prefix: string;
@@ -23,16 +48,14 @@ export const Pagination: NextPage<{
 }> = ({ base, prefix, totalCount, firstPageOffset, focused }) => {
   prefix = formatPrefix(prefix);
   const max_page = Math.ceil(
-    (totalCount + firstPageOffset) / ARTICLES_PER_PAGE,
+    (totalCount + firstPageOffset) / ARTICLES_PER_PAGE
   );
-  const range = (start: number, end: number) =>
-    [...Array(end - start + 1)].map((_, i) => start + i);
 
   return (
     <nav className="w-full font-bold">
       <ul className="mt-5 mb-16 flex items-center justify-center">
         {focused > 1 ? (
-          <li>
+          <li key="prev">
             <SquareIconButton
               iconColor="slate-900"
               border={false}
@@ -43,9 +66,13 @@ export const Pagination: NextPage<{
             />
           </li>
         ) : null}
-        {range(1, max_page).map((number, index) =>
-          focused === index + 1 ? (
-            <li key="prev">
+        {generatePageNumbersToShow(max_page, focused).map((number) =>
+          number === 0 ? (
+            <li key={number.toString()}>
+              <div className="w-10 h-12 mx-1 grid place-items-center">...</div>
+            </li>
+          ) : focused === number ? (
+            <li key={number.toString()}>
               <SquareTextButton
                 iconColor="slate-900"
                 border={false}
@@ -64,7 +91,7 @@ export const Pagination: NextPage<{
                 text={number.toString()}
               />
             </li>
-          ),
+          )
         )}
         {focused < max_page ? (
           <li key="next">
@@ -72,7 +99,7 @@ export const Pagination: NextPage<{
               iconColor="slate-900"
               border={false}
               focus={false}
-              href={`${prefix}${focused - 1}`}
+              href={`${prefix + +(focused + 1)}`}
               icon={faAngleRight}
               label="Prev"
             />
